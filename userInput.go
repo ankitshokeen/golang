902cc390883f2d45d2e2cc6d5bce820320af6c7a
ankitshokeen <ankitshokeen2001@gmail.com>
2025-08-17 03:48:39 +0530

    taking input from user by creating reader variable

---
+++ userInput.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	var welcome = "welcome to user input"
+	fmt.Println(welcome)
+
+	// creating reader variable
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter the rating for my learning skills")
+
+	// taking input form user
+	rating, _ := reader.ReadString('\n')
+	fmt.Println("thanks for rating: ", rating)
+	fmt.Printf("type of rating is %T \n", rating)
+}
